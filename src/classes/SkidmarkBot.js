36fe7f00423dd0bcbd7980a653eb09f04c6f0c51
casey <cnitz1802@gmail.com>
2025-03-01 16:21:59 -0600

    better LLM config

--- src/classes/SkidmarkBot.js
+++ src/classes/SkidmarkBot.js
@@ -1,8 +1,21 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 const BOT_USER_ID = '@' + process.env.DISCORD_BOT_ID;
 const CHANNEL_ID = process.env.NODE_ENV == 'production' ? 
     process.env.DISCORD_GENERAL_CHANNEL : process.env.DISCORD_DEV_CHANNEL;
+const MODEL = "gemini-2.0-flash";
+
+const SYSTEM_INSTRUCTIONS = 
+    "Your name is Chorley, and you are a bot designed to chat with users in a Discord channel for a simulator racing league. " +
+    "Your first name is Lee, but you generally don't refer to yourself by that name. " +
+    "You are powered by the Gemini API, which uses a generative model to create human-like responses. " +   
+    "You are designed to respond to messages that mention you, and you are programmed to respond to specific prompts. " +
+    "Each message you receive comes from a user in that chat, and is structured the following: 'username >> message'. " +
+    "Remember who sends what message to you based on the username from the beginning of every message. " +
+    "You have the personality of 1976 F1 World Champion James Hunt, and are in a bad mood. " +
+    "You are disgusted by the state of modern racing, and you are very opinionated. " +
+    "A user has sent you the following message, be open to conversation but brief and blunt.";
 
 module.exports = (() => {
     _ = new WeakMap();
@@ -12,22 +25,23 @@ module.exports = (() => {
 
             let obj = {
                 botClient : new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }),
-                model : genAI.getGenerativeModel({ model: "gemini-1.5-pro" }),
+                model : genAI.getGenerativeModel({ model: MODEL, systemInstruction: SYSTEM_INSTRUCTIONS }),
                 genAI,
                 isInit : false
             }
+            obj.aiChat = obj.model.startChat();
             _.set(this, obj);
         }
 
-        get chat(){
-            return _.get(this).chat;
+        get discordChat(){
+            return _.get(this).discordChat;
         }
 
         init(){
             if(!_.get(this).isInit){
                 _.get(this).botClient.on('ready', () => {
                     console.log(`Logged in as ${_.get(this).botClient.user.tag}!`);
-                    _.get(this).chat = _.get(this).botClient.channels.cache.get(CHANNEL_ID); 
+                    _.get(this).discordChat = _.get(this).botClient.channels.cache.get(CHANNEL_ID); 
                 });
                    
                 // Log In our bot
@@ -42,7 +56,7 @@ module.exports = (() => {
                             msg.reply(`Hello ${msg.author.username}`);
                         }
                         else if( msg.content.indexOf(BOT_USER_ID) !== -1 ){
-                            this.geminiGeneralChat(msg.content);
+                            this.geminiGeneralChat(msg.author.username, msg.content);
                         }
                     }
                 });
@@ -55,20 +69,17 @@ module.exports = (() => {
         _errorHandler(err){
             console.log("Gemini Error:");
             console.error(err);
-            _.get(this).chat.send("Sorry, I'm having technical difficulties at the moment.");
+            _.get(this).discordChat.send("Sorry, I'm having technical difficulties at the moment.");
         }
 
-        async geminiGeneralChat(usermsg){
+        async geminiGeneralChat(username,usermsg){
             try {
-              const prompt = "You have the personality of 1976 F1 World Champion James Hunt, and are in a bad mood."
-                +"A user has sent you the following message in quotes, be open to conversation but brief and blunt:\"" + usermsg + "\"";
-              
-              const result = await _.get(this).model.generateContent(prompt);
+              const result = await _.get(this).aiChat.sendMessage(username + " >> " + usermsg);
               const response = result.response;
               
               let text = response.text();
               text = text.replace("\"",""); // remove excess quotes in string
-              _.get(this).chat.send(text);
+              _.get(this).discordChat.send(text);
             } catch(err) {
                 this._errorHandler(err);
             }
@@ -76,11 +87,11 @@ module.exports = (() => {
         async sendLeagueUpdateMessage(){
             try {            
               const prompt = "Briefly describe a multi-car wreck in a junior racing series from the 1970s. Pretend you are former racer James Hunt, disgusted by what you've just seen."
-              const result = await _.get(this).model.generateContent(prompt);
+              const result = await _.get(this).aiChat.sendMessage(prompt);
 
               const response = result.response;
               const text = response.text();
-              _.get(this).chat.send(text);
+              _.get(this).discordChat.send(text);
             } catch(err) {
               this._errorHandler(err);
             }
